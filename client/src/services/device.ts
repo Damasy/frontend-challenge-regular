d1cb763b34d74aba66cf3877bf62fc5d7dfb0ae1
Ahmed Damasy <ahmeddamasy93@gmail.com>
2021-11-30 01:43:33 +0200

    handle active state request

--- client/src/services/device.ts
+++ client/src/services/device.ts
@@ -7,3 +7,14 @@ export const getAllDevices = () => {
 export const getDeviceById = (id: string) => {
   return fetch(API_URL + '/' + id);
 }
+
+export const toggleActiveState = (id: string, state: Boolean) => {
+  return fetch(API_URL + '/' + id, {
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    method: 'PATCH',                                                              
+    body: JSON.stringify( { active: state } )                                        
+  })
+}
